2fb4ac5c8152cc1c67dd6cb9dd635fc8ccd150ff
programmer-kapoor <shubhamkapoor9798@gmail.com>
2018-04-08 12:59:38 +0530

    footer

--- Ui-test/js/j1.js
+++ Ui-test/js/j1.js
@@ -1,6 +1,42 @@
 $(document).ready(function(){
     
+  window.sr = ScrollReveal();
+// JavaScript
+
+
+sr.reveal('.scrol-1',{
+               origin:'left',
+               
+               duration:2000,
+               
+                
+              });
+
+
+sr.reveal('.scrol-2',{
+  origin:'right',
+  
+   duration:2000,
+ 
     
+});
+
+
+
+    sr.reveal('.scrol-nav',{
+          origin:'bottom',
+          
+          duration:2000,
+});
+
+sr.reveal('.about-contant',{
+  origin:'top',
+  
+  duration:2000,
+});
+
+
+
 
 
   });
