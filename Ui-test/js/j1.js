04a5881cfa2cde54b7c1b10266af1ed5c9237a56
programmer-kapoor <shubhamkapoor9798@gmail.com>
2018-04-12 21:53:53 +0530

    cool

--- Ui-test/js/j1.js
+++ Ui-test/js/j1.js
@@ -4,36 +4,36 @@ $(document).ready(function(){
 // JavaScript
 
 
-sr.reveal('.scrol-1',{
-               origin:'left',
+// sr.reveal('.scrol-1',{
+//                origin:'left',
                
-               duration:1500,
+//                duration:1500,
                
                 
-              });
+//               });
 
 
-sr.reveal('.scrol-2',{
-  origin:'right',
+// sr.reveal('.scrol-2',{
+//   origin:'right',
   
-   duration:1500,
+//    duration:1500,
  
     
-});
+// });
 
 
 
-    sr.reveal('.scrol-nav',{
-          origin:'bottom',
+//     sr.reveal('.scrol-nav',{
+//           origin:'bottom',
           
-          duration:1500,
-});
+//           duration:1500,
+// });
 
-sr.reveal('.about-contant',{
-  origin:'top',
+// sr.reveal('.about-contant',{
+//   origin:'top',
   
-  duration:1500,
-});
+//   duration:1500,
+// });
 
 
 $("#event").click(function() {
